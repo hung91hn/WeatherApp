71486c728583d01b0b69184d592eff39aebbd2c3
Your Name <chung08061995@gmail.com>
2016-12-03 22:08:04 +0700

    SettingPreference

--- app/src/main/java/com/android1/weather/SettingActivity.java
+++ app/src/main/java/com/android1/weather/SettingActivity.java
@@ -1,42 +1,93 @@
 package com.android1.weather;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.ToggleButton;
+
+import com.android.volley.misc.DiskLruCache;
 
 public class SettingActivity extends AppCompatActivity {
 
     ToggleButton btnThongbao,btnAnhdong,btnDonvi;
+    SharedPreferences s1,s2,s3;
+    boolean on ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         setTitle("Thiết lập");
+
         addControls();
-        addEvents();
+
+        savePreference();
+
     }
 
-    private void addEvents() {
-        btnDonvi.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+    private void savePreference() {
+
+
+        s1=getSharedPreferences("abc",MODE_PRIVATE);
+        btnDonvi.setChecked(s1.getBoolean("Donvi",true));
+        btnDonvi.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-             if (compoundButton.isChecked());
-                else {
-                 Intent intent=getIntent();
-                 Bundle bundle=intent.getBundleExtra("TEMP");
-                 String s=bundle.getString("temp");
-                int i= (int) ((Integer.valueOf(s)-32)/1.8);
-                 Bundle bundle1=new Bundle();
-                 bundle1.putInt("tempC",i);
-                 Intent intent1=new Intent(SettingActivity.this,MainActivity.class);
-                 intent1.putExtra("TEMPC",bundle);
-                 startActivity(intent1);
-             }
+            public void onClick(View view) {
+                if (btnDonvi.isChecked()) {
+                    SharedPreferences.Editor editor = (SharedPreferences.Editor) getSharedPreferences("abc", MODE_PRIVATE).edit();
+                    editor.putBoolean("Donvi", true);
+                    editor.commit();
+                } else {
+                    SharedPreferences.Editor editor = (SharedPreferences.Editor) getSharedPreferences("abc", MODE_PRIVATE).edit();
+                    editor.putBoolean("Donvi", false);
+                    editor.commit();
+                }
+
             }
         });
+        s2=getSharedPreferences("edf",MODE_PRIVATE);
+        btnThongbao.setChecked(s2.getBoolean("Thongbao",true));
+        btnThongbao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (btnThongbao.isChecked()) {
+                    SharedPreferences.Editor editor = (SharedPreferences.Editor) getSharedPreferences("edf", MODE_PRIVATE).edit();
+                    editor.putBoolean("Thongbao", true);
+                    editor.commit();
+                } else {
+                    SharedPreferences.Editor editor = (SharedPreferences.Editor) getSharedPreferences("edf", MODE_PRIVATE).edit();
+                    editor.putBoolean("Thongbao", false);
+                    editor.commit();
+                }
+
+            }
+        });
+        s3=getSharedPreferences("xyz",MODE_PRIVATE);
+        btnAnhdong.setChecked(s3.getBoolean("Anhdong",true));
+        btnAnhdong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (btnAnhdong.isChecked()) {
+                    SharedPreferences.Editor editor = (SharedPreferences.Editor) getSharedPreferences("xyz", MODE_PRIVATE).edit();
+                    editor.putBoolean("Anhdong", true);
+                    editor.commit();
+                } else {
+                    SharedPreferences.Editor editor = (SharedPreferences.Editor) getSharedPreferences("xyz", MODE_PRIVATE).edit();
+                    editor.putBoolean("Anhdong", false);
+                    editor.commit();
+                }
+
+            }
+        });
+
+
     }
+
 
     private void addControls() {
         btnDonvi= (ToggleButton) findViewById(R.id.btnDonvi);
